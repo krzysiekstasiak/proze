e0f7f8dba50209fcae045dbd57e725d72092378b
Maciek <Maciek@192.168.1.111>
2014-06-15 18:24:10 +0200

    Merge origin/master
    Conflicts:
    	Test/src/PROZE/GUI/TestForm.java

--- Test/src/PROZE/GUI/EventListeners/GroupManagerListener.java
+++ Test/src/PROZE/GUI/EventListeners/GroupManagerListener.java
@@ -14,14 +14,38 @@ import EntitiesModels.UserEntity;
  * @author Krzysztof
  */
 public interface GroupManagerListener {
-
+    /**
+     * Tworzy nową grupę
+     * 
+     * @param name nazwa tworzonej grupy
+     */
     public void groupCreated(String name);
-
+   
+    /**
+     * Dokonyje aktualizacji grupy
+     * 
+     * @param group aktualizowana grupa
+     */
     public void groupUpdated(GroupEntity group);
-
+    
+    /**
+     * Dodaje nowego użytkownika do grupy
+     * 
+     * @param user dodawany użytkownik
+     */
     public void userAdded(UserEntity user);
-
+    
+    /**
+     * Usuwa użytkownika z grupy
+     * 
+     * @param user usuwany użytkownik
+     */
     public void userRemoved(UserEntity user);
-
+    
+    /**
+     * Usuwa test z grupy
+     * 
+     * @param test usuwany test
+     */
     public void testRemoved(TestDescription test);
 }
