c910d33296c8bc8a414b8cc314848a10c09ae5d4
Maciek <Maciek@192.168.1.111>
2014-06-15 18:42:47 +0200

    Merge origin/master
    Conflicts:
    	Test/src/PROZE/GUI/TestForm.java

--- Test/src/PROZE/GUI/EventListeners/NavigationListener.java
+++ Test/src/PROZE/GUI/EventListeners/NavigationListener.java
@@ -14,19 +14,55 @@ import EntitiesModels.TestDescription;
  */
 public interface NavigationListener {
 
+    /**
+     * Przechodzi do panelu podgląd profilu
+     * 
+     */
     public void navigatedToViewProfile();
 
+    /**
+     * Przechodzi do panelu moje grupy
+     * 
+     */
     public void navigatedToMyGroups();
 
+    /**
+     * Przechodzi do panelu moje testy
+     * 
+     */
     public void navigatedToMyTests();
 
+    /**
+     * Przechodzi do panelu wyszukiwania
+     * 
+     */
     public void navigatedToSearch();
 
+    /**
+     * Przechodzi do panelu powiadomień
+     * 
+     */
     public void navigatedToNotifications();
 
+    /**
+     * Przechodzi do panelu zarządzania testem
+     * 
+     * @param editedTest test którzy bedzie modyfikowany
+     * @param createNewTest czy modyfikowany test jest nowo tworzonym testem
+     */
     public void navigatedToTestManager(TestDescription editedTest, boolean createNewTest);
 
+    /**
+     * Przechodzi do panelu zarządzania grupą
+     * 
+     * @param group grupa która będzie modyfikowana 
+     * @param createNewGroup czy modyfikowana grupa jest nowo tworzoną grupą
+     */
     public void navigatedToGroupManager(GroupEntity group, boolean createNewGroup);
 
+    /**
+     * Przechodzi do wylogowania
+     * 
+     */
     public void navigatedToLogout();
 }
