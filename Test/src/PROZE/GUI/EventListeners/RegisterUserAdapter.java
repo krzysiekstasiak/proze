8dd8c5da5ee003b5c450afd85fa66a00abb9f38c
Maciek <Maciek@192.168.1.111>
2014-06-15 17:11:36 +0200

    Merge origin/master
    Conflicts:
    	Test/src/PROZE/GUI/TestForm.java

---
+++ Test/src/PROZE/GUI/EventListeners/RegisterUserAdapter.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PROZE.GUI.EventListeners;
+
+
+import EntitiesModels.UserEntity;
+
+/**
+ *
+ * @author Krzysztof
+ */
+public class RegisterUserAdapter implements RegisterUserListener {
+
+    @Override
+    public void registerConfirmed(String name, String secondName, String login, String email, char[] password ) {
+    }
+
+   
+}
